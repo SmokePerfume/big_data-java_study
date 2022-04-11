fcd9076f5e9333db56a8a1ce60432dc8fec2d7d0
SmokePerfume <wjdgnstls97@naver.com>
2022-04-11 09:45:26 +0900

    각 스레드 모듈화와 성공 실패 카드의 앞 뒷면 색상 추가

--- java_study/src/com/econ/thread/L06CardGame.java
+++ java_study/src/com/econ/thread/L06CardGame.java
@@ -1,16 +1,28 @@
 package com.econ.thread;
 
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Random;
+import java.awt.Color;
+import java.awt.event.*;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-
+interface CardColor{
+	Color FRONT=new Color(239, 255, 125);
+	Color FRONT_TEXT=new Color(0, 0, 0);
+	
+	Color BACK=new Color(145, 145, 145);
+	Color BACK_TEXT=new Color(0, 0, 0);
+	
+	Color FAILED=new Color(255, 0, 0);
+	Color FAILED_TEXT=new Color(255, 255, 255);
+	
+	Color SUCCESS=new Color(200, 240, 200);
+	Color SUCCESS_TEXT=new Color(15, 115, 25);	
+}
 class CardGameFrame extends JFrame{
 	Card[] cards=new Card[12];
 	Integer [] cards_nums= {1,1,2,2,3,3,4,4,5,5,6,6};
@@ -23,55 +35,9 @@ class CardGameFrame extends JFrame{
 			add(c); 
 		}
 		
-		Thread t1=new Thread(new Runnable() {
-			public void run() {
-				for(Card c :cards) {
-					try {Thread.sleep(100);} catch (InterruptedException e) {e.printStackTrace();}
-					c.setText(c.card_num+"");
-					f.validate();
-				}
-			}
-		});
-		Thread t2=new Thread(new Runnable() {//다시 가리기	(숫자 지우기)
-			@Override
-			public void run() {
-				for(Card c :cards) {
-					try {Thread.sleep(100);} catch (InterruptedException e) {e.printStackTrace();}
-					c.setText("");
-					f.validate();
-				}
-			}
-		});
-		Thread t3=new Thread(new Runnable() {
-			@Override
-			public void run() {
-				//선택한 두개의 카드가 같은지 .5초마다 무한히 검사하는 코드 
-				while(true) {
-					try {Thread.sleep(500);} catch (InterruptedException e1) {	e1.printStackTrace();}
-					if(click_cards.size()==2) { 
-						//실패했을 때 다시 뒤집기
-						if(click_cards.get(0).card_num!=click_cards.get(1).card_num) {
-							try {Thread.sleep(500);} catch (InterruptedException e1) {e1.printStackTrace();}
-							for(Card c: click_cards) {
-								c.setText(c.card_num+"");	
-							}
-							try {Thread.sleep(200);} catch (InterruptedException e1) {	e1.printStackTrace();}								
-							for(Card c: click_cards) {
-								c.setText("");								
-							}
-						}else{
-						//성공했을 때 다시 뒤집어지지 않도록 true로 바꾸기
-							for(Card c: click_cards) {
-								c.success=true;
-								c.setText(c.card_num+"");					
-							}
-						}
-						click_cards.clear();	
-						//성공하든 실패하든 2번 클릭하고 나면 클릭 초기화
-					}
-				}
-			}
-		});
+		Thread t1=new ShowCards();
+		Thread t2=new HideCards();
+		Thread t3=new SuccesCards();
 		this.setLayout(new GridLayout(3,4));
 		this.setBounds(600,100,600,800);
 		this.setVisible(true);
@@ -87,6 +53,67 @@ class CardGameFrame extends JFrame{
 		
 		
 		
+		
+	}
+
+	class ShowCards extends Thread {
+		public void run() {
+			for(Card c :cards) {
+				try {Thread.sleep(100);} catch (InterruptedException e) {e.printStackTrace();}
+				c.setText(c.card_num+"");
+				c.setBackground(CardColor.FRONT);
+				c.setForeground(CardColor.FRONT_TEXT);	
+				f.validate();
+			}
+		}
+	}
+	
+	class HideCards extends Thread {
+		@Override
+		public void run() {
+			for(Card c :cards) {
+				try {Thread.sleep(100);} catch (InterruptedException e) {e.printStackTrace();}
+				c.setText("");
+				c.setBackground(CardColor.BACK);
+				c.setForeground(CardColor.BACK_TEXT);	
+				f.validate();
+			}
+		}
+	}
+	
+	
+	class SuccesCards extends Thread {
+		@Override
+		public void run() {
+			while(true) { //카드 .5초마다 무한히 검사하는 코드 
+				try {Thread.sleep(500);} catch (InterruptedException e1) {	e1.printStackTrace();}
+				if(click_cards.size()==2) { 
+					//실패했을 때 다시 뒤집기
+					if(click_cards.get(0).card_num!=click_cards.get(1).card_num) { //카드가 다름 
+						try {Thread.sleep(500);} catch (InterruptedException e1) {e1.printStackTrace();}
+						for(Card c: click_cards) {
+							c.setText(c.card_num+"");	
+							c.setBackground(CardColor.FAILED);
+							c.setForeground(CardColor.FAILED_TEXT);	
+						}
+						try {Thread.sleep(200);} catch (InterruptedException e1) {	e1.printStackTrace();}								
+						for(Card c: click_cards) {
+							c.setText("");		
+							c.setBackground(CardColor.BACK);
+							c.setForeground(CardColor.BACK_TEXT);	
+						}
+					}else{ //카드가 같음 
+						for(Card c: click_cards) {
+							c.success=true;
+							c.setText(c.card_num+"");	
+							c.setBackground(CardColor.SUCCESS);
+							c.setForeground(CardColor.SUCCESS_TEXT);	
+						}
+					}
+					click_cards.clear();	//2번 클릭 후 초기화
+				}
+			}
+		}
 	}
 	
 	public void randomCards() {//링크리스트 변환 후 그 크기안에서 랜덤한 위치의 숫자를 뽑아냄
