844e8619208541169d8a25773074f74c034d5342
SmokePerfume <wjdgnstls97@naver.com>
2022-04-12 22:47:06 +0900

    마우스를 올렸을때 누르고 있을 때의 이벤트 추가 (이벤트 중복 발생으로 색이 재변화 하는것을 막을 필요가 있음-> 성공후 이벤트 지우기)

--- java_study/src/com/econ/myproject/CardGame.java
+++ java_study/src/com/econ/myproject/CardGame.java
@@ -21,11 +21,16 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
+
 interface CardColor{ //전부 상수처리됨
+	Color ACTIVE=new Color(220, 245, 115);
+	Color OVER=new Color(145, 145, 145);
+	
 	Color FRONT=new Color(239, 255, 125);
 	Color FRONT_TEXT=new Color(0, 0, 0);
 	
-	Color BACK=new Color(145, 145, 145);
+
+	Color BACK=new Color(175, 175, 175);
 	Color BACK_TEXT=new Color(0, 0, 0);
 	
 	Color FAILED=new Color(255, 0, 0);
@@ -40,8 +45,6 @@ class CardGameFrame extends JFrame{
 	AtomicInteger time=new AtomicInteger(30);
 	public static int score=0;
 	public static int succes_cnt=0;
-	public static boolean ready=true;
-	public static boolean cnt_run=false;
 	JLabel timeL=new JLabel(time.get()+"초");
 	JLabel scoreL=new JLabel(score+"점");
 	Container cont = getContentPane(); 
@@ -72,14 +75,16 @@ class CardGameFrame extends JFrame{
 		header.add(timeL);
 		header.add(scoreL);
 		
-		main.setLayout(new GridLayout(4,6,10,10)); 
+		main.setLayout(new GridLayout(4,6,10,10));  //cards_nums변경시 출력화면 변경필요
 		footer.add(startBtn);
 		
 		cont.add(header,BorderLayout.NORTH);
 		cont.add(main,BorderLayout.CENTER);
 		cont.add(footer,BorderLayout.SOUTH);
+		
 		this.setBounds(0,0,400,500); 
 		this.setVisible(true); 
+		
 		startBtn.addActionListener(new StartBtnHandler());
 	}
 	
@@ -89,31 +94,29 @@ class CardGameFrame extends JFrame{
 			new Thread() {
 				public void run() {
 				startBtn.setEnabled(false);
-				System.out.println("시작버튼 작동");
 				randomCards();
 				clearCard();
 				cardset();
 				score=0;
 				scoreL.setText(score+"점");
-				ready=false;
-				
+				if(t1!=null)t1.stop();			
+				if(t2!=null)t2.stop();	
+				if(t3!=null)t3.stop();	
+				if(t4!=null)t4.stop();	
 				t1=new ShowCards();
 				t2=new HideCards();
 				t3=new SuccesCards();
 				t4=new Cntdown();
-				cnt_run=true;
 				//t1~t2까지 순서대로 진행 후 t1,t2 끝나면 t3, t4동시에 시작
 				t1.start();
 				try {t1.join();} catch (InterruptedException e1) {e1.printStackTrace();}
 				t2.start();
 				try {t2.join();} catch (InterruptedException e2) {e2.printStackTrace();}
 				for(Card c: cards) { //버튼에 이벤트 추가
-					c.addActionListener(new ChoiceCard(c));
+					c.addMouseListener(new ChoiceCard(c));
 					c.setEnabled(true);
 				}
-
 				startBtn.setEnabled(true);
-				cnt_run=false;
 				t3.start();
 				t4.start();
 				}
@@ -136,6 +139,8 @@ class CardGameFrame extends JFrame{
 		int i=0;
 		for(Card c :cards) {  
 			c.setEnabled(false);
+			c.setOpaque(true);
+			c.setBorderPainted(false);
 			main.add(c); 
 			cards_clone[i++]=c;
 		}
@@ -147,27 +152,18 @@ class CardGameFrame extends JFrame{
 			time.set(30);
 			timeL.setText(time+"초");
 			while(time.get()>0) {
-				System.out.println("Cntdown 스레드 실행중");
-				if(cnt_run) { 
-					System.out.println("기존 Cntdown 스레드 꺼짐");
-					this.interrupt();	
-				}
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					System.out.println("카운트 오류");
-					return;
-				}
-				if(succes_cnt>=12) {
+				try {Thread.sleep(1000);} catch (InterruptedException e) {return;}
+				if(succes_cnt>=cards_nums.length) {
 					JOptionPane.showMessageDialog(null,"소요시간:"+ time+"초 / 총 점수: "+(score+(time.get()*5)));
 					for(Card c :cards) {
 						c.setEnabled(false);
 					}
 					succes_cnt=0;
 					break;
+				}else {
+					time.set(time.get()-1); 
+					timeL.setText(time+"초");
 				}
-				time.set(time.get()-1); 
-				timeL.setText(time+"초");
 			}
 			if(time.get()<=0&&succes_cnt<cards_nums.length) { 
 				JOptionPane.showMessageDialog(null,"실패");
@@ -208,12 +204,6 @@ class CardGameFrame extends JFrame{
 		@Override
 		public void run() {
 			while(true) { //카드 .5초마다 무한히 검사하는 코드 
-				System.out.println("succes 스레드 실행중");
-				if(cnt_run||succes_cnt>=cards_nums.length) { 
-					System.out.println("기존 succes 스레드 꺼짐");
-					this.interrupt();
-					break;
-				}
 				try {Thread.sleep(500);} catch (InterruptedException e1) {	e1.printStackTrace();}
 				if(click_cards.size()==2) { 
 					//실패했을 때 다시 뒤집기
@@ -260,19 +250,38 @@ class CardGameFrame extends JFrame{
 	}
 	
 	//카드 선택시
-	class ChoiceCard implements ActionListener{
+	class ChoiceCard extends MouseAdapter{
 		Card c;
 		public ChoiceCard(Card c) {
 			this.c=c;
 		}
 		@Override
-		public void actionPerformed(ActionEvent e) {
+		public void mouseClicked(MouseEvent e) {
 			if(c.success) return; //성공한 카드를 선택 
 			if(click_cards.size()<2) {//성공한 카드를 제외하고 선택 & 숫보여줌 같은 카드면 취소
 				if(click_cards.size()==1 && c==click_cards.get(0) ) return; 
 				click_cards.add(c);
 				c.setText(c.card_num+"");
 			}
+		}
+		public void mousePressed(MouseEvent e) {
+			JButton c=(JButton)e.getSource();
+			c.setBackground(CardColor.ACTIVE);
+		}
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			JButton c=(JButton)e.getSource();
+			c.setBackground(CardColor.BACK);
+		}
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			JButton c=(JButton)e.getSource();
+			c.setBackground(CardColor.OVER);
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			JButton c=(JButton)e.getSource();
+			c.setBackground(CardColor.BACK);
 		}
 
 	}
